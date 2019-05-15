cc05ead3b995da23988dc5d94a3446d3be61d6fb
Arslan Gait <arslan.gait@nu.edu.kz>
2019-05-15 18:20:56 +0600

    code refactor

---
+++ bot_handlers.py
@@ -0,0 +1,19 @@
+import pyowm
+from bot import bot
+
+owm = pyowm.OWM(OWM_API_KEY)
+
+@bot.message_handler(content_types=['text'])
+def send_weather(message):
+    place = message.text
+    try:
+        w = observation.get_weather()
+    except:
+        bot.reply_to(message, 'Not found')
+        return
+    temp = w.get_temperature('celsius')['temp']
+    answer = f'Weather in {place}: \n{temp} °C, ' + w.get_detailed_status() + '\n'
+    bot.reply_to(message, answer)
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
--- run_server.py
+++ run_server.py
@@ -3,23 +3,6 @@ from telebot import types
 from config import *
 from bot_handlers import bot
 import os
-import pyowm, telebot
-
-owm = pyowm.OWM(OWM_API_KEY)
-
-bot = telebot.TeleBot(TOKEN)
-
-@bot.message_handler(content_types=['text'])
-def send_weather(message):
-    place = message.text
-    try:
-        w = observation.get_weather()
-    except:
-        bot.reply_to(message, 'Not found')
-        return
-    temp = w.get_temperature('celsius')['temp']
-    answer = f'Weather in {place}: \n{temp} °C, ' + w.get_detailed_status() + '\n'
-    bot.reply_to(message, answer)
 
 server = flask.Flask(__name__)
 
